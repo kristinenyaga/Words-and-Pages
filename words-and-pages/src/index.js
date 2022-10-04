f0a0f844e4144553629558c87362d02eebb4c7f9
krisine <kristine.nyaga@student.moringaschool.com>
2022-10-05 00:44:28 +0300

    wraps the App.js in context file

--- words-and-pages/src/index.js
+++ words-and-pages/src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import "bootstrap/dist/css/bootstrap.min.css"
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { UserProvider } from "../context/user";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+   <UserProvider>
     <App />
-  </React.StrictMode>
+   </UserProvider>
+   
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
