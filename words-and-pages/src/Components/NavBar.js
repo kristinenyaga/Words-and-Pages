1a2304597950bcde9fd2a0dd46aeaff540ee40f6
krisine <kristine.nyaga@student.moringaschool.com>
2022-10-06 18:21:13 +0300

    moves links  to home page

--- words-and-pages/src/Components/NavBar.js
+++ words-and-pages/src/Components/NavBar.js
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom";
 function NavBar() {
   const linkStyles = {
     display: "inline-block",
-    width: "80px",
+    width: "120px",
     padding: "12px",
     margin: "0 6px 6px",
     background: "black",
@@ -22,14 +22,22 @@ function NavBar() {
       >
         Login
       </NavLink>
-      <NavLink
-        to="words-and-pages/src/Components/Home.js"
+      {/* <NavLink
+        to="words-and-pages/src/Components/Home.js/*"
         exact="true"
         style={linkStyles}
         
       >
        Home
       </NavLink>
+      <NavLink
+        to="words-and-pages/src/Components/MyBooks.js"
+        exact="true"
+        style={linkStyles}
+        
+      >
+       SearchBook
+      </NavLink> */}
     </div>
   );
 }
