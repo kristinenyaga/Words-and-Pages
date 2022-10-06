5f0114379e80aba8e4332c1e04199d74c193ea80
krisine <kristine.nyaga@student.moringaschool.com>
2022-10-06 18:13:16 +0300

    adds state and onchange functionalities to the form

--- words-and-pages/src/Components/SearchForm.js
+++ words-and-pages/src/Components/SearchForm.js
@@ -1,16 +1,27 @@
-import { useEffect, useState,useContext } from "react";
+import React, { useState } from "react";
 import {FaSearch} from "react-icons/fa";
-import { DetailsProvider } from "../Context/Context";
-function SearchForm(){
-  const {setSearchItem} = useContext(DetailsProvider);
+function SearchForm({sendSearchItem}){
 
+  const [searchnewItem,setSearchNewItem]=useState("")
+  function handleSubmit(e){
+    e.preventDefault()
+    sendSearchItem(searchnewItem)
+    
+  }
+
+  function handleChange(searchedResult){
+   setSearchNewItem(searchedResult)
+  }
+
+  
   return(
-    <div className='search-form'>
+    <div className='search-form'onSubmit={handleSubmit}>
     <div className='container'>
       <div className='search-form-content'>
         <form className='search-form' >
           <div className='search-form-elem flex flex-sb bg-white'>
-            <input type = "text" className='form-control' placeholder='Pride and Prejudice...'  />
+            <input type = "text" className='form-control' placeholder='Pride and Prejudice...' 
+            value={searchnewItem} onChange={(e)=>handleChange(e.target.value)} />
             <button type = "submit" className='flex flex-c' >
               <FaSearch className='text-purple' size = {32} />
             </button>
