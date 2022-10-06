595667966d8c09d4417cebd6511f6a922cac7f18
krisine <kristine.nyaga@student.moringaschool.com>
2022-10-06 18:43:38 +0300

    adds component to display books to read and a section to add a book to the reading list

--- words-and-pages/src/Components/Home.js
+++ words-and-pages/src/Components/Home.js
@@ -1,18 +1,60 @@
-import React from "react";
-import SearchForm from "./SearchForm";
+import React, { useEffect, useState } from "react";
+import AllBooks from "./AllBooks";
+import YourReadingList from "./YourReadingList";
+import { NavLink } from "react-router-dom";
+
 
 function Home(){
+  const [myBooks,setMyBooks]=useState([])
+  const [readingList,setReadingList]=useState([])
+  function addBookToList(bookadded){
+    setReadingList([...readingList,bookadded])
+    console.log(bookadded)
+  }
+  useEffect(()=>{
+    fetch( 'https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=newest&key=AIzaSyD92Au7-Uf5gi2Gh7wAbAJhTfaMqpiM53Q'+'&maxResults=40')
+    .then(response => response.json())
+    .then(response => setMyBooks(response.items))
+
+  },[])
+  const linkStyles = {
+    display: "inline-block",
+    width: "120px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "black",
+    textDecoration: "none",
+    color: "white",
+  };
   return(
-    <div className='holder'>
-        <header className='header'>
-            {/* <Navbar /> */}
-            <div className='header-content flex flex-c text-center text-white'>
-                <h2 className='header-title text-capitalize'>Good books are just like good friends.</h2><br />
-                <p className='header-text fs-18 fw-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam beatae sapiente quibusdam consequatur perspiciatis facere laboriosam non nesciunt at id repudiandae, modi iste? Eligendi, rerum!</p>
-                <SearchForm />
-            </div>
-        </header>
+    <>
+    <div className="display-flex">
+    <NavLink
+        to="/words-and-pages/src/Components/Home.js/"
+        exact="true" style={linkStyles}> Home
+      </NavLink>
+      <NavLink
+        to="/words-and-pages/src/Components/MyBooks.js"
+        exact="true" style={linkStyles}>SearchBook
+      </NavLink>
+
     </div>
+    
+    
+    <YourReadingList readingList={readingList}/>
+    <h1 className="text-muted text-center">Yohooo....Get your books Here</h1>
+    <figure className="text-center">
+      <blockquote className="blockquote">
+        <p>If you don’t like to read, you haven’t found the right book.</p>
+      </blockquote>
+      <figcaption className="blockquote-footer">
+          <cite title="Source Title">J.K. Rowling</cite>
+      </figcaption>
+    </figure>
+    <AllBooks books={myBooks} addBookToList={addBookToList} />
+    </>
+   
+
   
   )
 }
