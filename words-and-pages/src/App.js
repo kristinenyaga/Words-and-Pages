8089cb410e595a43fe0226255b9632e404094c8e
krisine <kristine.nyaga@student.moringaschool.com>
2022-10-06 18:11:47 +0300

    wraps the app component in the context provider

--- words-and-pages/src/App.js
+++ words-and-pages/src/App.js
@@ -5,15 +5,19 @@ import "./App.css";
 import Login from './Components/Login';
 import NavBar from './Components/NavBar';
 import Home from './Components/Home';
-
+import MyBooks from './Components/MyBooks'
+import EachBookDetails from './Components/EachBookDetails';
 
 function App() {
  return (
   <Router>
      <NavBar />
       <Routes>
-       <Route path="/" element={<Login />} />
-       <Route path="words-and-pages/src/Components/Home.js" element={<Home />} />
+       <Route exact path="/" element={<Login />} />
+       <Route exact path="/words-and-pages/src/Components/MyBooks.js/" element={<MyBooks />} />
+       <Route exact path="/words-and-pages/src/Components/Home.js/" element={<Home />} />
+       <Route exact path="/words-and-pages/src/Components/EachBookDetails.js/" element={<EachBookDetails />} />
+       
                 
       </Routes>
   </Router>
