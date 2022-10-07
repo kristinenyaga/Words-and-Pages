f36aab926d7fcd6fadf5a600f24af3b8711dc6ff
krisine <kristine.nyaga@student.moringaschool.com>
2022-10-07 07:56:18 +0300

    updates the function to include the callbacks

--- words-and-pages/src/Components/AllBooks.js
+++ words-and-pages/src/Components/AllBooks.js
@@ -1,12 +1,12 @@
 
 import BookContainer from "./BookContainer"
 
-function AllBooks({books,addBookToList}){
+function AllBooks({books,addBookToList,removeBookFromList}){
   return(
     <>
      <div className="row d-flex justify-content-around">
      {books.map(book =>{
-      return <BookContainer key={book.id} book={book} addBookToList={addBookToList}/>
+      return <BookContainer key={book.id} book={book} addBookToList={addBookToList} removeBookFromList={removeBookFromList}/>
      }
     )}
 
--- words-and-pages/src/Components/BookContainer.js
+++ words-and-pages/src/Components/BookContainer.js
@@ -1,6 +1,12 @@
-function BookContainer({book,addBookToList}){
+import { BsFillFileExcelFill } from "react-icons/bs";
+function BookContainer({book,addBookToList,removeBookFromList}){
   function handleClick(book){
     addBookToList(book)
+  }
+  function handleDelete(deleteBook){
+    
+    // /e.target.parentNode.parentNode.parentNode.parentNode.remove()
+    removeBookFromList(deleteBook)
   }
   return(
     <div className="card mb-3 flex-direction-parent shadow-lg p-3 mb-5 bg-body rounded d-grid gap-3 px-2 "   >
@@ -10,10 +16,11 @@ function BookContainer({book,addBookToList}){
         </div>
         <div className="col-md-8">
           <div className="card-body">
-            <h5 className="card-title">TITLE: {book.volumeInfo.title}</h5>
-            <h4 className="card-title">SUBTITLE: {book.volumeInfo.subtitle}</h4>
-            <h6>PUBLISHEDATE: {book.volumeInfo.publishedDate}</h6>
-            <p>PAGECOUNT: {book.volumeInfo.pageCount}</p>
+          <BsFillFileExcelFill onClick={()=>handleDelete(book)} className='text-purple' size = {32} />
+            <h4 className="card-title text-muted text-center"><span className="text-dark">TITLE:</span> {book.volumeInfo.title}</h4>
+            <h5 className="card-title"> <span className="text-primary">SUBTITLE:</span> {book.volumeInfo.subtitle}</h5>
+            <h6><span className="text-primary">PUBLISHEDATE: </span>{book.volumeInfo.publishedDate}</h6>
+            <p><span className="text-primary">PAGECOUNT:</span> {book.volumeInfo.pageCount}</p>
             <button type="button" className="btn btn-dark" onClick={()=>handleClick(book)}>
              Add to YourReadingList
             </button>
