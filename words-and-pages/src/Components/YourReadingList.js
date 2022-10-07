eef12ce61ee14ed709bf2345dafcb51aaebdc4ca
krisine <kristine.nyaga@student.moringaschool.com>
2022-10-07 07:54:54 +0300

    including the removebook from list function

--- words-and-pages/src/Components/YourReadingList.js
+++ words-and-pages/src/Components/YourReadingList.js
@@ -1,14 +1,14 @@
 import BookContainer from "./BookContainer"
-function YourReadingList({readingList}){
+function YourReadingList({readingList,removeBookFromList}){
   return(
-    <div className="ui segment inverted olive bot-army">
+    <div className="ui segment inverted olive book-list">
       <div className="ui five column grid">
       <h1 className="text-center ">YourReadingList</h1>
 
-        <div className="row bot-army-row gap-3">
+        <div className="row book-list-row gap-3">
        
           {readingList.map(book =>{
-              return <BookContainer key={book.id} book={book}/>
+              return <BookContainer key={book.id} book={book} removeBookFromList={removeBookFromList}/>
           })}
       
         </div>
