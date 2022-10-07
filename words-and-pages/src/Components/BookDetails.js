6ab4cdb1d8432535ec504f9edfc4022b342da767
krisine <kristine.nyaga@student.moringaschool.com>
2022-10-07 07:56:59 +0300

    removes the route to the eachbookdetail component

--- words-and-pages/src/Components/BookDetails.js
+++ words-and-pages/src/Components/BookDetails.js
@@ -1,14 +1,12 @@
-
-import BookItem from "./BookItem";
-import EachBookDetails from "./EachBookDetails";
-import {Route, Routes} from "react-router-dom";
+import BookItem from "./BookItem"
 import {useNavigate} from 'react-router-dom'
-import { useState,useRef } from "react";
+import { useState} from "react";
 
 function BookDetails({books}){
   const navigate=useNavigate()
   const [newBooks,setBook]=useState([])
-  console.log(newBooks)
+
+
   function getBookId(book){
   const returnedbooks= [...newBooks,book]
     setBook(returnedbooks)
@@ -18,10 +16,10 @@ function BookDetails({books}){
    
 
   }
-  <EachBookDetails book={newBooks}/>
+ 
   return (
     <>
-    <h1>Results</h1>
+    <h1 className="text-muted text-center">Results</h1>
     <div className="row d-flex justify-content-around">
     {books.map(book =>{
       return <BookItem key={book.id} book={book} getBookId={getBookId}/>
--- words-and-pages/src/Components/BookItem.js
+++ words-and-pages/src/Components/BookItem.js
@@ -1,21 +1,21 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css"
-import { Link } from "react-router-dom";
-function BookItem({book,getBookId}){
+function BookItem({book,getBookId,thumbnail="https://images.unsplash.com/photo-1519682337058-a94d519337bc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",amount}){
   
   return(
     <>
     <div className="card mb-3 flex-direction-parent shadow-lg p-3 mb-5 bg-body rounded d-grid gap-3 px-2"  >
       <div className="row g-0 flex-direction">
         <div className="col-md-4">
-            <img src={book.volumeInfo.imageLinks.smallThumbnail} className="img-fluid rounded-start" alt="..."/>
+            <img src={thumbnail} className="img-fluid rounded-start" alt="..."/>
         </div>
         <div className="col-md-8">
           <div className="card-body">
-            <h5 className="card-title">TITLE: {book.volumeInfo.title}</h5>
-            <h4 className="card-title">SUBTITLE: {book.volumeInfo.subtitle}</h4>
-            <h6>PUBLISHEDATE: {book.volumeInfo.publishedDate}</h6>
-            <p>PAGECOUNT: {book.volumeInfo.pageCount}</p>
+            <h4 className="card-title text-muted text-center"><span className="text-dark">TITLE:</span> {book.volumeInfo.title}</h4>
+            <h5 className="card-title"><span className="text-primary">SUBTITLE:</span>{book.volumeInfo.subtitle}</h5>
+            <h6><span className="text-primary">PUBLISHEDATE: </span> {book.volumeInfo.publishedDate}</h6>
+            <p><span className="text-primary">PAGECOUNT:</span> {book.volumeInfo.pageCount}</p>
+            <p className="amount">{amount}</p>
             <button type="button" className="btn btn-dark" onClick={()=>getBookId(book)}>More
             </button>
 
