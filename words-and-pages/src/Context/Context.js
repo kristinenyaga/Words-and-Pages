188b1f97b9e7267f6e90f502aeaf98404b692fe9
krisine <kristine.nyaga@student.moringaschool.com>
2022-10-07 07:48:34 +0300

    updating context to include boolean of logged in

--- words-and-pages/src/Context/Context.js
+++ words-and-pages/src/Context/Context.js
@@ -1,55 +1,18 @@
-import React,{ useState,useEffect,useContext } from "react";
+import React,{ useState} from "react";
 
 const DetailsContext = React.createContext();
 function DetailsProvider({ children }) {
 
-  const [books,setBooks]=useState([])
-  // const[loading,setLoading]=useState("true")
-
-  const URL = "http://openlibrary.org/search.json?title="
-  const [searchItem,setSearchItem]=useState("the lost world")
-
-
-  useEffect(()=>{
-    fetch(`${URL}${searchItem}`)
-    .then(response => response.json())
-    .then(data =>{
-      const {docs}=data
-      if(docs){
-        const newBooks= docs.slice(0,20).map((bookItem)=>{
-          const {key,author_name,edition_count,cover_i,title,number_of_pages_median} = bookItem
-          return{
-            key:key,
-            author_name:author_name,
-            coverId:cover_i,
-            title:title,
-            editionCount:edition_count,
-            numberOfPages:number_of_pages_median
-
-          }
-        })
-        setBooks(newBooks)
-      }
-      else{
-        setBooks([])
-      }
-    })
-    .catch(error => console.log(error))
-  },[searchItem])
-
+  const [logIn,setIsLogged]=useState(true)
   
     return (
-      <DetailsContext.Provider value = {{
-         books, setSearchItem
-      }}>
+      <DetailsContext.Provider value ={{ logIn, setIsLogged }}>
           {children}
       </DetailsContext.Provider>
   
   )
 }
 
-export const useGlobalContext = () => {
-  return useContext(DetailsContext);
-}
+
 
 export {DetailsContext, DetailsProvider};
